e67b90a0d7c05da08808c909d9d79bc664c2743e
XieZongChen <952937570@qq.com>
2024-01-08 11:39:41 +0800

    feat: 实现静态文件服务

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 use axum::{response::Html, routing::get, Router};
+use tower_http::services::{ServeDir, ServeFile};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    // 配置当访问不存在 url 时的默认返回
+    let serve_dir =
+        ServeDir::new("assets2").not_found_service(ServeFile::new("assets2/index.html")); // not_found_service 传入的是默认获取的文件
 
-    // run it
+    // 使用路由构建应用程序
+    let app = Router::new()
+        .route("/", get(handler))
+        .nest_service("/assets", ServeDir::new("assets")) // 把 /assets/* 的 URL 映射到 assets 目录下
+        .nest_service("/assets2", serve_dir.clone())
+        .fallback_service(serve_dir); // 注意需要挂载
+
+    // 启动端口监听
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
+    
     axum::serve(listener, app).await.unwrap();
 }
 
